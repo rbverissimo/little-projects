445f1db8d7c9b44340103fc8456ff48de93271a8
rbverissimo <rverissimo.dev@gmail.com>
2022-11-08 10:57:30 -0300

    reverted some of the label placing, added the label for entry()

--- qrcode-tkinter/main.py
+++ qrcode-tkinter/main.py
@@ -20,12 +20,16 @@ heading_label = Label(heading_frame, text='Gerador de QR Code', bg='ghostwhite',
                       font=('Times', 20, 'bold'))
 heading_label.place(relx=0, rely=0, relwidth=1, relheight=1)
 
-# label for input:
+# label for input --- needs to align center with the title
 frame_input = Frame(wn, bg='ghostwhite', bd=5)
-frame_input.place(relx=0.15, rely=0.15, relwidth=0.7, relheight=0.3)
+frame_input.place(relx=0.1, rely=0.15, relwidth=0.7, relheight=0.3)
 label_input = Label(frame_input, text='Digite a URL:', fg='black',
                     font=('Times', 16, 'bold'))
-label_input.place(relx=0.2, rely=0.2, relheight=0.1)
+label_input.place(relx=0.05, rely=0.2, relheight=0.08)
+
+# just for the place holder 
+text = Entry(frame_input, font='Century 12')
+text.place(relx=0.1, rely=0.35, relwidth=0.7, relheight=0.3)
 
 wn.mainloop()
 
